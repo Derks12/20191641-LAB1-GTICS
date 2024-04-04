d2280654f06c87624c42a2f69765c8748ea39436
Alexander Abrisqueta <a20191641@pucp.edu.pe>
2024-04-03 20:35:29 -0500

    first commit

---
+++ src/main/java/org/example/a20191641lab1gtics/Application.java
@@ -0,0 +1,13 @@
+package org.example.a20191641lab1gtics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/a20191641lab1gtics/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.a20191641lab1gtics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
